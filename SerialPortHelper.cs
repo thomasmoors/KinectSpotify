7699abfc4bb2bcefb90ad571dce7da3d93e69252
Thomas Moors <thomasmoors1992@gmail.com>
2017-11-28 17:42:18 +0100

    commit missing Helper file

---
+++ SerialPortHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace Microsoft.Samples.Kinect.DiscreteGestureBasics
+{
+    public static class SerialPortHelper
+    {
+      public static void SendBytesOverCom(SerialPort sp, string message)
+        {
+            byte[] byteArray = System.Text.Encoding.UTF8.GetBytes(message);
+            sp.Write(byteArray, 0, byteArray.Length);
+        }
+    }
+}
